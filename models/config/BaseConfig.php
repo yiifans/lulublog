6771c8bce41c5c4c92474f22fc6ed1b038620cbe
yiifans <yiifans@sina.com>
2015-02-08 11:02:33 +0800

    format

--- models/config/BaseConfig.php
+++ models/config/BaseConfig.php
@@ -6,42 +6,42 @@ use Yii;
 use app\core\base\BaseModel;
 use app\models\Config;
 
-
 class BaseConfig extends BaseModel
 {
+
 	public function initAll()
 	{
 		$this->initAllInternael();
 	}
-	
+
 	public function saveAll()
 	{
 		$this->saveAllInternael();
 	}
-	
+
 	protected function getConfigKeys()
 	{
-		//return $this->attributes();
+		// return $this->attributes();
 		return array_keys($this->attributeLabels());
 	}
-	
+
 	protected function initAllInternael()
 	{
 		$keys = $this->getConfigKeys();
-		foreach ($keys as $key)
+		foreach($keys as $key)
 		{
 			$this->initOneInternael($key);
 		}
 	}
-	
+
 	protected function initOneInternael($key, $defaultValue = '')
 	{
-		$model  = Config::findOne(['`key`'=>$key]);
+		$model = Config::findOne(['`key`' => $key]);
 		if($model != null)
 		{
 			$this->$key = $model->value;
 		}
-		else 
+		else
 		{
 			$model = new Config();
 			$model->key = $key;
@@ -51,18 +51,18 @@ class BaseConfig extends BaseModel
 			$this->$key = $defaultValue;
 		}
 	}
-	
+
 	protected function saveAllInternael()
 	{
 		$keys = $this->getConfigKeys();
-		foreach ($keys as $key)
+		foreach($keys as $key)
 		{
 			$this->saveOneInternael($key, $this->$key);
 		}
 	}
-	
-    protected function saveOneInternael($key,$value)
-    {
-    	Config::updateAll(['value'=>$value], ['`key`'=>$key]);
-    }
+
+	protected function saveOneInternael($key, $value)
+	{
+		Config::updateAll(['value' => $value], ['`key`' => $key]);
+	}
 }
